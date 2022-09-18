7100b89bd39433da8d4d9c48a04d0b760ad6ca73
SpencerNinja <spencer.ninja@gmail.com>
2022-09-18 11:03:46 -0600

    Created initial project files

---
+++ CorePractice/CorePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CorePractice
+//
+//  Created by Spencer Hurd on 9/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
